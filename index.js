863ac9bf346680aed6622b3a7fc730dcb321ab5a
Hafsat Ajia-Egbeyemi <hafsatajia@gmail.com>
2022-06-01 14:25:31 +0100

    "Added a variable and prompt"

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-alert("Do you know what day of the week it is?");
-prompt("What day is it?");
+let userName = prompt("What is your name?")
+
+alert( userName, "Do you know what day of the week it is?");
+
+prompt("Okay" userName, "What day is it?");
 alert('Are you sure? Let me check - "does scientific magic"');
 let weekday = new Date().toLocaleString("en-US", { weekday: "long" });
 // console.log(weekday);
 
 if (weekday=== "Monday") {
-    alert("It is Monday! Let's get the week started!");
+    alert(userName, "it is Monday! Let's get the week started!");
 } else if (weekday==="Tuesday") {
-    alert("It is Tuesday! So lets keep hustling!");
+    alert(userName, "it is Tuesday! So lets keep hustling!");
 } else if (weekday ==="Wednesday") {
-    alert("It is Wednesday! TGIW?...nah, doesn't sound right, so keep working! :-)")
+    alert(userName,"it is Wednesday! TGIW?...nah, doesn't sound right, so keep working! :-)")
 } else if (weekday === "Thursday") {
-    alert ("Thursday! Ayyyyy! You are one day closer to Friyay! Have a wonderful work day");
+    alert (userName, "it is Thursday! Ayyyyy! You are one day closer to Friyay! Have a wonderful work day");
 } else if (weekday==="Friday") {
-    alert ("It is Friyayyyy! Here is to the weekend's almost here!")
-} else ("WEEKEND! YAYYY!")
+    alert (userName, "it is Friyayyyy! Here is to the weekend's almost here!")
+} else (userName, "WEEKEND! YAYYY!")
